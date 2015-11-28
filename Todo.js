66169e4c72c0d102557d24ad6e4c6968c3dcb709
Victor Ionescu <victorpionescu@gmail.com>
2015-11-28 21:35:44 +0100

    Fixes everything. Add + delete works

--- Todo.js
+++ Todo.js
@@ -13,67 +13,89 @@ function  TodoItem(name, Date, desc, remind, rating) {
 			+ "] ")};
 	}
 			
-	var list = [];
+var list = [];
+var counter = 0;
+		
+TodoItem.prototype.setName = function(name) {this.name = name;};
+TodoItem.prototype.setDate = function(Date) {this.Date = Date;};
+TodoItem.prototype.getName = function() {return this.name;};
+TodoItem.prototype.getDate = function() {return this.Date;};
+TodoItem.prototype.setDesc = function(desc) {this.desc = desc;};
+TodoItem.prototype.getDesc = function() {return this.desc;};
+TodoItem.prototype.setRemind = function(remind) {this.remind = remind;};
+TodoItem.prototype.getRemind = function() {return this.remind;};
+TodoItem.prototype.setRating = function(rating) {this.rating = rating;};
+TodoItem.prototype.getRating = function() {return this.rating;};
 			
-	TodoItem.prototype.setName = function(name) {this.name = name;};
-	TodoItem.prototype.setDate = function(Date) {this.Date = Date;};
-	TodoItem.prototype.getName = function() {return this.name;};
-	TodoItem.prototype.getDate = function() {return this.Date;};
-	TodoItem.prototype.setDesc = function(desc) {this.desc = desc;};
-	TodoItem.prototype.getDesc = function() {return this.desc;};
-	TodoItem.prototype.setRemind = function(remind) {this.remind = remind;};
-	TodoItem.prototype.getRemind = function() {return this.remind;};
-	TodoItem.prototype.setRating = function(rating) {this.rating = rating;};
-	TodoItem.prototype.getRating = function() {return this.rating;};
 			
+function addItem() {
+	var item = new TodoItem("", "");
+	item.setName(document.getElementById("namein").value);
+	item.setDesc(document.getElementById("descin").value);
+	item.setDate(document.getElementById("datein").value);
+	item.setRemind(document.getElementById("remindin").value);
+	item.setRating(document.getElementById("ratingin").value);
+		
+	// add todo to the list
+	list.push(item);
+		
+	// crear text field
+	document.getElementById("namein").value = "";
+	document.getElementById("descin").value = "";
+	document.getElementById("datein").value = "";
+	document.getElementById("remindin").value = "";
+	document.getElementById("ratingin").value = "";
+	
+	// give notification
+	//alert("item added to list!");
+		
+	console.log(list.toString());
+	
+	var content = "Name: " + list[counter].name + 
+	"<br>Description: " + list[counter].desc +
+	"<br>Date: " + list[counter].Date +
+	"<br>Reminder: " + list[counter].remind +
+	"<br>Rating: " + list[counter].rating +
+	"<br> <button class=\"delete\" id=\"delete" + counter + "\" onclick=\"delItem(" + counter + ");\"> Delete</button>" +
+	"<button class=\"edit\">Edit</button>" +
+	"<button class=\"done\">Done</button>";
+	
+	var newDiv = document.createElement("div");
+	newDiv.id = "viewdiv" + counter;
+	newDiv.className = "viewclass";
+	
+	var br = document.createElement("br");
+	br.id = "divBreak" + counter;
+	
+	document.getElementById("viewer").appendChild(newDiv);
+	document.getElementById("viewer").appendChild(br);
+	document.getElementById("viewdiv" + counter).innerHTML = content;
+	counter++;
+}
 			
-	function addItem() {
-		var item = new TodoItem();
-		item.setName(document.getElementById("namein").value);
-		item.setDesc(document.getElementById("descin").value);
-		item.setDate(document.getElementById("datein").value);
-		item.setRemind(document.getElementById("remindin").value);
-		item.setRating(document.getElementById("ratingin").value);
-		list.push(item);
-		alert("item added to list!");
+function delItem(index) {
+	//splice removes a specific index, pop removes the latest. syntax: splice("index", "howmanyelements")
+	list.splice(index, 1);
+	
+	var div = document.getElementById("viewdiv" + index);
+	document.getElementById("viewer").removeChild(div);
+	
+	var br = document.getElementById("divBreak" + index);
+	document.getElementById("viewer").removeChild(br);
+	
+	counter--;
+	cleanup(index);
+}
+			
+//resets the id's to match their new position
+function cleanup(index) {
+	for(var i = index + 1; i<list.length + 1; i++) {
+		//changes the onclick function parameter to match its position
+		document.getElementById("delete" + i).setAttribute( "onClick", "delItem(" + (i-1) + ");" );
+		document.getElementById("delete" + i).id = "delete" + (i-1);
+		document.getElementById("viewdiv" + i).id = "viewdiv" + (i-1);
+		document.getElementById("divBreak" + i).id = "divBreak" + (i-1);
+			
 	}
-			
-	function delItem(index) {
-		list.pop(index);
-		var div = document.getElementById("viewdiv" + index);
-		document.getElementById("viewer").removeChild(div);
-	}
-	var counter = 0;
-	function view() {
-		console.log(list.toString());
-		var content = "";
-		if(!(list.length == counter)) {
-		for(var i = 0; i < list.length; i++) {
-			content = "Name: " 	+ list[i].name + 
-			"<br>Description: " + list[i].desc +
-			"<br>Date: " 		+ list[i].Date +
-			"<br>Reminder: " 	+ list[i].remind +
-			"<br>Rating: " 		+ list[i].rating +
-			"<br> <button class=\"delete\" id=\"delete" + i + "\" onclick=\"delItem(" + i + ")\"> Delete</button>" +
-				"<button class=\"edit\">Edit</button>" +
-				"<button class=\"done\">Done</button>";
-				
-					//create div variable
-					var newDiv = document.createElement("div");
-					newDiv.id = "viewdiv" + i;
-					newDiv.className = "viewclass";
-					//create br variable
-					var br = document.createElement("br");
-					//insert div
-					document.getElementById("viewer").appendChild(newDiv);
-					//insert br
-					document.getElementById("viewer").appendChild(br);
-					//insert content in div
-					console.log("content: " + content);
-					document.getElementById("viewdiv" + i).innerHTML = content;
-					console.log("i: " + i);
-					counter = list.length;
-		}
-		}	
-	}
+}
 			
